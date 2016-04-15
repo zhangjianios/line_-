df7bff0674fd81da9a0fce8a7a615ac52656f6d5
mac <mac@macdemac-mini.local>
2016-04-15 15:19:59 +0800

    Initial Commit

---
+++ line_指示线/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  line_指示线
+//
+//  Created by mac on 16/4/15.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ line_指示线Tests/line____Tests.m
@@ -0,0 +1,39 @@
+//
+//  line____Tests.m
+//  line_指示线Tests
+//
+//  Created by mac on 16/4/15.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface line____Tests : XCTestCase
+
+@end
+
+@implementation line____Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
